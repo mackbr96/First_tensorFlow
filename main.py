1943a9d20102f815f13ea0fa4c2e8913e341ea7e
Ben <mackbr@clarkson.edu>
2018-09-24 20:44:49 -0400

    sdsdfsdf

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import tensorflow as tf
     
 mnist = tf.keras.datasets.mnist
+print(mnist)
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
 
 x_train = tf.keras.utils.normalize(x_train, axis=1)
 x_test = tf.keras.utils.normalize(x_test, axis=1)
 
 model = tf.keras.models.Sequential()
 model.add(tf.keras.layers.Flatten())
+
 model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
 model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
 
@@ -24,3 +27,17 @@ model.fit(x_train, y_train, epochs=4)
 val_loss, val_acc = model.evaluate(x_test, y_test)
 
 print(val_loss, val_acc)
+
+
+model.save('save.model')
+new_model = tf.keras.models.load_model('save.model')
+
+predic = new_model.predict([x_test])
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(np.argmax(predic[1]))
+
+plt.imshow(x_test[1])
+plt.show()
